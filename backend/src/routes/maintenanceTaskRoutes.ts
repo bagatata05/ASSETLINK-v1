88c95ed09bab08278e2a9528ec962c3f99ee3c59
Naphier Awalie <naphiera@gmail.com>
2026-04-16 01:07:01 +0800

    Update maintenanceTaskRoutes.ts

--- backend/src/routes/maintenanceTaskRoutes.ts
+++ backend/src/routes/maintenanceTaskRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { maintenanceTaskController } from '../controllers/MaintenanceTaskController';
-import { verifyToken } from '../middleware/authMiddleware';
+import { verifyToken, checkRole } from '../middleware/authMiddleware';
 import { asyncErrorWrapper } from '../middleware/asyncErrorWrapper';
 
 const router = Router();
@@ -9,7 +9,7 @@ router.use(verifyToken);
 
 router.get('/', asyncErrorWrapper(maintenanceTaskController.getAll));
 router.get('/:id', asyncErrorWrapper(maintenanceTaskController.getById));
-router.post('/', asyncErrorWrapper(maintenanceTaskController.create));
-router.patch('/:id', asyncErrorWrapper(maintenanceTaskController.update));
+router.post('/', checkRole(['principal']), asyncErrorWrapper(maintenanceTaskController.create));
+router.patch('/:id', checkRole(['maintenance', 'principal']), asyncErrorWrapper(maintenanceTaskController.update));
 
 export default router;
